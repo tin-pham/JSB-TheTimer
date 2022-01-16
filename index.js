1294b94a4684de3b223ec18ae345dacb24895bc0
shin <heoconlinhat123@gmail.com>
2022-01-16 13:09:28 +0700

    Main completed

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+class Timer {
+  constructor(timeInput, startButton, pauseButton) {
+    this.timeInput = timeInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
 
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+
+  start = () => {
+    this.intervalId = setInterval(this.tick, 1000);
+  };
+
+  tick = () => {
+    if (this.currentTime <= 0) return;
+    this.currentTime = this.currentTime - 1;
+  };
+
+  pause = () => {
+    clearInterval(this.intervalId);
+  };
+
+  get currentTime() {
+    return parseInt(this.timeInput.value);
+  }
+
+  set currentTime(time) {
+    this.timeInput.value = time;
+  }
+}
+
+const timeInput = document.querySelector('.timer-input');
+const startButton = document.querySelector('.start-button');
+const pauseButton = document.querySelector('.pause-button');
+const timer = new Timer(timeInput, startButton, pauseButton);
